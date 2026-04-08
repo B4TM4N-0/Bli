41521f3a36bb0e8c881deee8cb6872c8f69d9851
B4TM4N-0 <batmangrahhh@gmail.com>
2026-04-09 00:35:24 +0800

    Update TopBar.vue

--- components/layout/TopBar.vue
+++ components/layout/TopBar.vue
@@ -1,10 +1,11 @@
 <template>
   <header class="sticky top-0 z-40 border-b border-white/10 bg-[#0f0f11]/95 backdrop-blur-xl">
-    <div class="mx-auto flex max-w-7xl items-center justify-between px-4 py-2 sm:px-6 lg:px-8">
+    <div class="mx-auto flex max-w-7xl items-center justify-between px-4 py-3 sm:px-6 lg:px-8">
+      
       <div class="text-2xl font-black tracking-tight text-white">Xyzu</div>
 
       <button class="text-white/70 transition hover:text-white">
-        <span class="sr-only">menu</span>
+        <span class="sr-only">Open menu</span>
         <Icon name="fa7-solid:bars" class="h-6 w-6" />
       </button>
 
